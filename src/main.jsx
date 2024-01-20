74e547353740b402796568675aa966dfad056bef
Alade C. Yessoufou <aladecharaf23@gmail.com>
2024-01-20 17:09:38 +0100

    config: necessary packages

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
 import Root from './routes/root';
 
 const router = createBrowserRouter([
